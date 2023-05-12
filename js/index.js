b2d806f36b2cd4e0fb0c3bcc3b8e3c9c6e9f1bb4
Pierre221352 <221352@virtualwindow.co.za>
2023-05-12 12:03:44 +0200

    Exercise one complete

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var textName = "Pierre";
+var textSurname = "Ferreira";
+var integerAge = "20";
+const subject = "Development";
+
+console.log("Hello, my name is " + textName + " " + textSurname + " I am " + 20 + " years old and I am studying " + subject);
+
