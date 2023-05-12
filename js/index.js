29b9a2b6aa5d28d8c2b9d05858e7eb1b1546b746
Pierre221352 <221352@virtualwindow.co.za>
2023-05-12 13:26:06 +0200

    exercise two

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ const subject = "Development";
 
 console.log("Hello, my name is " + textName + " " + textSurname + " I am " + 20 + " years old and I am studying " + subject);
 
+{let numOne = 5;
+let numTwo = 6;
+let numThree = 3;
+let numFour = 2;
+let answer = 0;
+
+console.log("Answer = " + numOne + numTwo / numThree * numFour)}
+
+
